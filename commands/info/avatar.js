44701d07bbcd208346c699ba1bb1422c95d25a0f
usmanyt13 <54153109+usmanyt13@users.noreply.github.com>
2019-09-20 23:26:57 +0500

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'avatar',    
     run: async (client, message, args) => {
-        message.reply('Here Is Your Avatar',message.author.avatarURL);
+        message.reply(message.author.avatarURL);
 
     }
 }
