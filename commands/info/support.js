badd8241204ac25f9bf561ea4cc6d09fff77bd9b
usmanyt13 <54153109+usmanyt13@users.noreply.github.com>
2019-09-24 22:57:57 +0500

    Add files via upload

---
+++ commands/info/support.js
@@ -0,0 +1,15 @@
+const { RichEmbed, Client } = require('discord.js');
+
+module.exports = {
+    name:'support',
+    run: async (client, message, args) => {
+        const sembed = new RichEmbed()
+        .setTitle('Support Server')
+        .setColor('#59b6eb')
+        .setDescription(`Support Server Is Available Soon You Can Contact 
+        @!Usmanᵛᵉʳᶦᶠᶦᵉᵈ✓#1496
+         For Any Kind of Help `);
+        
+        message.channel.send(sembed);
+
+    }}
