1b4ff6fb14112f822a106bd1a7b4cfa1fa25c54c
usmanyt13 <54153109+usmanyt13@users.noreply.github.com>
2020-09-14 20:21:14 +0500

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,7 +1,25 @@
 module.exports = {
     name:'avatar',    
     run: async (client, message, args) => {
-        message.reply(message.author.avatarURL, `Here Is Your Avatar` );
+        if(message.content === `u!avatar`){
+      let user;
+
+      if(message.mentions.users.first()) {
+          user = message.mentions.users.first();
+      
+      }else  {
+          user = message.author;
+      };
+
+      let av = user.displayAvatarURL({dynamic:true, size:4096 });
+      const embed = new Discord.MessageEmbed()
+      .setTitle (`${user.tag}`)
+      .setDescription(`**Avatar**`)
+      .setColor('#000000')
+      .setURL(av)
+      .setImage(av)
+    message.channel.send(embed);
+    }
 
     }
 }
