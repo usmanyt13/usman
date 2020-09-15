4b6c7cc7eb2a8734bc11b29afb2ab40de6617c00
usmanyt13 <54153109+usmanyt13@users.noreply.github.com>
2020-09-15 14:20:27 +0500

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -5,11 +5,13 @@ module.exports = {
     name:'serverinfo',
     run: (client, message, args) => {
         let sicon = message.guild.iconURL;
+        let owne = message.guild.ownerID;
     let serverembed = new RichEmbed()
     .setDescription("Server Information")
     .setColor("#15f153")
     .setThumbnail(sicon)
     .addField("Server Name", message.guild.name)
+    .addField("Server Owner", `<@${owne}>`)
     .addField("Created On", message.guild.createdAt)
     .addField("You Joined", message.member.joinedAt)
     .addField("Total Members", message.guild.memberCount);
